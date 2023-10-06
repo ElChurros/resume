880e9f12c32eceacb123090941d20668ee15a8da
Arthur LEMAIRE <arthur.lemaire@epitech.eu>
2023-10-06 12:17:38 +0200

    preload express logo aswell

--- src/components/ImageLoader/index.js
+++ src/components/ImageLoader/index.js
@@ -6,13 +6,14 @@ import projects from '../../assets/projects.png'
 import html from '../../assets/logos/html-logo.png'
 import css from '../../assets/logos/css-logo.png'
 import javascript from '../../assets/logos/javascript-logo.png'
+import nodejs from '../../assets/logos/nodejs-logo.png'
 import cpp from '../../assets/logos/cpp-logo.png'
 import python from '../../assets/logos/python-logo.png'
 import php from '../../assets/logos/php-logo.png'
 import react from '../../assets/logos/react-logo.png'
 import vue from '../../assets/logos/vue-logo.png'
 import angular from '../../assets/logos/angular-logo.png'
-import nodejs from '../../assets/logos/nodejs-logo.png'
+import express from '../../assets/logos/express-logo.png'
 import django from '../../assets/logos/django-logo.png'
 import symfony from '../../assets/logos/symfony-logo.png'
 import socketio from '../../assets/logos/socketio.png'
@@ -39,13 +40,14 @@ const ImageLoader = () => {
     new Image().src = html
     new Image().src = css
     new Image().src = javascript
+    new Image().src = nodejs
     new Image().src = cpp
     new Image().src = python
     new Image().src = php
     new Image().src = react
     new Image().src = vue
     new Image().src = angular
-    new Image().src = nodejs
+    new Image().src = express
     new Image().src = django
     new Image().src = symfony
     new Image().src = androidstudio
